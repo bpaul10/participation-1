29832389be18fdeecaac4f0d647522bb5a1dc9a6
bernard <bpaul10@student.cscc.edu>
2026-09-15 17:58:54 -0400

    testing of sandwich order concept

--- 7-8.py
+++ 7-8.py
@@ -1,1 +1,9 @@
 sandwich_orders = ["grilled chicken sandwich", "fried chicken sandwich", "bad boiled chicken sandwich", "deep fried chicken sandwich", "grilled cheese sandwich(with chicken)"]
+finished_sandwiches = []
+
+
+for sandwich in sandwich_orders:
+    print(" i have made one")
+    finished_sandwiches.append(sandwich)
+else:
+    print("all done")
